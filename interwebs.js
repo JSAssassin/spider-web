492655dbcb6cdeb7bb74ce0f4ca0a5f9b0702340
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-11-04 23:00:50 -0500

    document crawlUpThread

--- interwebs.js
+++ interwebs.js
@@ -1,11 +1,14 @@
 const Web = require('./web');
 
 class InterWebs {
-    crawlUpThread(thread) {
-        let prey = ['oprahbees.jpg'];
-        let spiral = ['http://beesbeesbees.com', 'http://beesbeesbees.com'];
-        return new Web(thread, prey, spiral);
-    }
+  /**
+   * Return a new web after crawling
+   */
+  crawlUpThread(thread) {
+    let prey = ['oprahbees.jpg'];
+    let spiral = ['http://beesbeesbees.com', 'http://beesbeesbees.com'];
+    return new Web(thread, prey, spiral);
+  }
 }
 
 module.exports = InterWebs;
